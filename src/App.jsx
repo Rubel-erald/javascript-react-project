6db92892b93c8b0676be79c104870b39455d37df
Rubel erald <your-rubelerald.j@gmail.com>
2026-04-24 09:46:57 +0530

    2 types of creating a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-function App() {
-  return <h1>hello</h1>;
-}
-function sum (){
-  return <p>hello hii</p>
-}
-export {App , sum};
+// creating components
+// there are 2 types of components
+// 1.functional components
+// 2.class components 
+
+// 1.functional components
+
+// function App (){
+//   return <>
+//   <h1>hello world !</h1>
+//   <p>hi i am rubel erald</p>
+//   </>
+// }
+
+// export default App;
+
+
+
+// // 2.class components
+// import React from "react";
+// class App extends React.Component{
+//     render() {
+//         return <>
+//         <h1>hello world !</h1>
+//         <p>hi i am rubel erald</p>
+//         </>
+//     }
+
+// }
+// export default App;
+
